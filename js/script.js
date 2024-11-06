69103de195766baf24a632446ac474047e69ba28
Pitri <p3maiyani@gmail.com>
2024-11-06 10:48:11 +0700

    all version 2

--- js/script.js
+++ js/script.js
@@ -12,12 +12,16 @@ function calculateBMI() {
 
     // Dapatkan nilai gender
     const gender = document.querySelector('input[name="jenkel"]:checked').value;
+    
 
     // Validasi input
-    if (isNaN(berat) || isNaN(tinggi) || tinggi <= 0 || berat <= 0) {
-        document.getElementById("result").innerText = "Silakan masukkan nilai berat dan tinggi yang valid.";
+    if (isNaN(berat) || isNaN(tinggi) || tinggi <= 0 || berat <= 0 ) {
+        document.getElementById("resultBMI").innerHTML= "<h4> Silahkan masukkan nilai berat dan tinggi yang valid.</h4>";
+        document.getElementById("adviceBMI").textContent = "";
         return;
     }
+
+
 
     // Perhitungan BMI
     const bmi = berat / (tinggi * tinggi);
@@ -25,35 +29,41 @@ function calculateBMI() {
 
     // Menentukan kategori BMI
     let category ;
+    let advice ; 
     if (bmi < 18.5) {
         category = "Kekurangan Berat Badan";
+        advice = "Anda berada dalam kategori berat badan kurang. Pertimbangkan untuk makan makanan bergizi dan menambah asupan kalori.";
     } else if (bmi >= 18.5 && bmi < 24.9) {
         category = "Normal (Sehat)";
+        advice = "Anda memiliki berat badan yang sehat. Pertahankan pola makan yang seimbang dan rutin berolahraga.";
     } else if (bmi >= 25 && bmi < 29.9) {
-        category = "Kelebihan Berat Badan";
+        category = "Kelebihan Berat Badan"
+        advice ="Anda memiliki sedikit kelebihan berat badan. Cobalah berolahraga secara teratur dan jaga pola makan untuk mencapai berat badan ideal.";
     } else {
         category = "Obesitas";
+        advice ="Hasil BMI Anda menunjukan obesitas. Sebaiknya Anda menjaga pola makan sehat dan berkonsultasi dengan profesional untuk bimbingan lebih lanjut.";
     }
 
     // Menampilkan hasil
-    // document.getElementById("result").innerText = `BMI Anda: ${bmi.toFixed(1)} (${category})`;
+    // document.getElementById("resultBMI").innerText = `BMI Anda: ${bmi.toFixed(1)} (${category})`;
 
     const header = document.createElement("h3");
     header.textContent = `Hasil Perhitungan BMI (${gender})`;
 
-    const resultText = document.createElement("p");
-    resultText.innerHTML = `Hasil BMI Anda ${roundedBMI} <br> Anda memiliki berat badan ${category}`;
+    const resultBMIText = document.createElement("p");
+    resultBMIText.innerHTML = `Hasil BMI Anda ${roundedBMI} <br> Anda memiliki berat badan ${category}`;
 
      // Temukan elemen div hasil
-     const resultDiv = document.getElementById("result");
+     const resultBMIDiv = document.getElementById("resultBMI");
 
-     // Kosongkan isi dari resultDiv untuk menghindari penumpukan hasil
-     resultDiv.innerHTML = "";
+     // Kosongkan isi dari resultBMIDiv untuk menghindari penumpukan hasil
+     resultBMIDiv.innerHTML = "";
  
-     // Tambahkan header, hasil, dan pesan gender ke resultDiv
-     resultDiv.appendChild(header);
-     resultDiv.appendChild(resultText);
+     // Tambahkan header, hasil, dan pesan gender ke resultBMIDiv
+     resultBMIDiv.appendChild(header);
+     resultBMIDiv.appendChild(resultBMIText);
+     document.getElementById("adviceBMI").textContent = advice;
      
-    // const resultDiv = document.getElementById("result");
-    // resultDiv.innerHTML = `Hasil BMI ${roundedBMI} <br> Anda memiliki berat badan ${category}`;
+    // const resultBMIDiv = document.getElementById("resultBMI");
+    // resultBMIDiv.innerHTML = `Hasil BMI ${roundedBMI} <br> Anda memiliki berat badan ${category}`;
 }
